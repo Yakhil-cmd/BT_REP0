62bcd893203ebb92c953a069c3b3e2bfab07076e
Yakhil <grasss.dev@gmail.com>
2026-07-22 19:22:53 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,5 +2,5 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "codertjay/2026-07-metric-dev-oyakhil-main"
-REPO_NAME = "2026-07-metric-dev-oyakhil-main"
+SOURCE_REPO = "kaiachain/kaia"
+REPO_NAME = "kaia"
