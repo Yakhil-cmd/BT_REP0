3c93bc84b0208b3ca88420fcdb2ebc21e8e24802
Yakhil <grasss.dev@gmail.com>
2026-08-02 21:42:41 +0100

    Update repository configuration values

--- questions.py
+++ questions.py
@@ -1,6 +1,6 @@
 import json
 import os
 
-MAX_REPO = 40
-SOURCE_REPO = "aptos-labs/aptos-core"
-REPO_NAME = "aptos-core"
+MAX_REPO = 25
+SOURCE_REPO = "polkadot-fellows/runtimes"
+REPO_NAME = "runtimes"
